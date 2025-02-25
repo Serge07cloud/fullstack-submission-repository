9065d22f78c4cbb8885c7ac24b8dc29a9429e961
Serge07cloud <wisdomandy.5@gmail.com>
2025-02-25 23:33:02 +0000

    Displaying statistics into an HTML table

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -15,9 +15,10 @@ const Feedback = ({ handleGood, handleNeutral, handleBad }) => {
 
 const StatisticLine = ({ text, value }) => {
   return (
-    <div>
-      {text} {value}
-    </div>
+    <tr>
+      <td>{text}</td>
+      <td>{value}</td>
+    </tr>
   );
 };
 
@@ -37,12 +38,16 @@ const Statistics = ({ good, neutral, bad }) => {
     return (
       <div>
         <h1>statistics</h1>
-        <StatisticLine text="good" value={good} />
-        <StatisticLine text="neutral" value={neutral} />
-        <StatisticLine text="bad" value={bad} />
-        <StatisticLine text="all" value={all} />
-        <StatisticLine text="average" value={average} />
-        <StatisticLine text="positive" value={positive + " %"} />
+        <table>
+          <tbody>
+            <StatisticLine text="good" value={good} />
+            <StatisticLine text="neutral" value={neutral} />
+            <StatisticLine text="bad" value={bad} />
+            <StatisticLine text="all" value={all} />
+            <StatisticLine text="average" value={average} />
+            <StatisticLine text="positive" value={positive + " %"} />
+          </tbody>
+        </table>
       </div>
     );
 };
