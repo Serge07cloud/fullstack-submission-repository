ff1c7bbaf6d66eec1670299f7ee1dc15b715e5ce
Serge07cloud <wisdomandy.5@gmail.com>
2025-10-25 23:55:29 +0000

    Add lint configuration

--- part3/phonebook_backend/index.js
+++ part3/phonebook_backend/index.js
@@ -1,101 +1,101 @@
-require("dotenv").config();
-const express = require("express");
-const Person = require("./models/person");
-const morgan = require("morgan");
-const app = express();
+require('dotenv').config()
+const express = require('express')
+const Person = require('./models/person')
+const morgan = require('morgan')
+const app = express()
 
-morgan.token("content", (request, response) => {
-  return JSON.stringify(request.body);
-});
+morgan.token('content', (request) => {
+  return JSON.stringify(request.body)
+})
 
-app.use(express.json());
+app.use(express.json())
 app.use(
   morgan(
-    ":method :url :status :res[content-length] - :response-time ms :content"
+    ':method :url :status :res[content-length] - :response-time ms :content'
   )
-);
+)
 
 const errorHandler = (error, request, response, next) => {
-  console.log(error.message);
+  console.log(error.message)
 
-  if (error.name === "CastError") {
-    return response.status(400).send({ error: "id not formatted properly" });
-  } else if (error.name === "ValidationError") {
-    return response.status(400).json({ error: error.message });
+  if (error.name === 'CastError') {
+    return response.status(400).send({ error: 'id not formatted properly' })
+  } else if (error.name === 'ValidationError') {
+    return response.status(400).json({ error: error.message })
   }
 
-  next(error);
-};
+  next(error)
+}
 
-const currentDateTime = () => new Date();
+const currentDateTime = () => new Date()
 
-app.get("/api/persons", (request, response) => {
+app.get('/api/persons', (request, response) => {
   Person.find({}).then((persons) => {
-    response.json(persons);
-  });
-});
+    response.json(persons)
+  })
+})
 
-app.get("/api/info", (request, response) => {
+app.get('/api/info', (request, response) => {
   Person.find({}).then((result) => {
     response.send(`
       <div>Phonebook has info for ${result.length} people</div>
       <p>${currentDateTime()}</p>
-      `);
-  });
-});
+      `)
+  })
+})
 
-app.get("/api/persons/:id", (request, response) => {
-  Person.findById(request.params.id).then((result) => response.json(result));
-});
+app.get('/api/persons/:id', (request, response) => {
+  Person.findById(request.params.id).then((result) => response.json(result))
+})
 
-app.delete("/api/persons/:id", (request, response) => {
+app.delete('/api/persons/:id', (request, response, next) => {
   Person.findByIdAndDelete(request.params.id)
     .then(() => response.status(204).end())
-    .catch((error) => next(error));
-});
+    .catch((error) => next(error))
+})
 
-app.post("/api/persons", (request, response, next) => {
-  const body = request.body;
+app.post('/api/persons', (request, response, next) => {
+  const body = request.body
 
   const person = new Person({
     name: body.name,
     number: body.number,
-  });
+  })
 
   person
     .save()
     .then((savedPerson) => {
-      response.json(savedPerson);
+      response.json(savedPerson)
     })
-    .catch((error) => next(error));
-});
+    .catch((error) => next(error))
+})
 
-app.put("/api/persons/:id", (request, response, next) => {
-  const { name, number } = request.body;
+app.put('/api/persons/:id', (request, response, next) => {
+  const { name, number } = request.body
 
   Person.findByIdAndUpdate(
     request.params.id,
     { name, number },
-    { new: true, runValidators: true, context: "query" }
+    { new: true, runValidators: true, context: 'query' }
   )
     .then((updatedPerson) => {
-      if (!updatedPerson) return response.status(404).end();
-      response.json(updatedPerson);
+      if (!updatedPerson) return response.status(404).end()
+      response.json(updatedPerson)
     })
-    .catch((error) => next(error));
-});
+    .catch((error) => next(error))
+})
 
 const unknownEndpoint = (request, response) => {
-  return response.status(404).send({ error: "bad request" });
-};
+  return response.status(404).send({ error: 'bad request' })
+}
 
 // Handler of requests with unknown endpoint
-app.use(unknownEndpoint);
+app.use(unknownEndpoint)
 // Handler of requests with result to error
-app.use(errorHandler);
+app.use(errorHandler)
 
-const PORT = process.env.PORT;
+const PORT = process.env.PORT
 
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
+  console.log(`Server running on port ${PORT}`)
+})
--- part3/phonebook_backend/models/person.js
+++ part3/phonebook_backend/models/person.js
@@ -1,17 +1,17 @@
-const mongoose = require("mongoose");
+const mongoose = require('mongoose')
 
-mongoose.set("strictQuery", false);
+mongoose.set('strictQuery', false)
 
-console.log("Connecting to database");
+console.log('Connecting to database')
 
-const url = process.env.MONGODB_URI;
+const url = process.env.MONGODB_URI
 
 mongoose
   .connect(url)
   .then(() => {
-    console.log("Connection succeeded");
+    console.log('Connection succeeded')
   })
-  .catch((error) => console.log("Connection failed:", error.message));
+  .catch((error) => console.log('Connection failed:', error.message))
 
 const personSchema = new mongoose.Schema({
   name: {
@@ -23,20 +23,20 @@ const personSchema = new mongoose.Schema({
     type: String,
     validate: {
       validator: function (v) {
-        return /^(\d{2,3})-\d{5,}$/.test(v);
+        return /^(\d{2,3})-\d{5,}$/.test(v)
       },
       message: (props) => `${props.value} is not a valid phone number.`,
     },
     required: true,
   },
-});
+})
 
-personSchema.set("toJSON", {
+personSchema.set('toJSON', {
   transform: (document, returnedObject) => {
-    returnedObject.id = returnedObject._id.toString();
-    delete returnedObject._id;
-    delete returnedObject.__v;
+    returnedObject.id = returnedObject._id.toString()
+    delete returnedObject._id
+    delete returnedObject.__v
   },
-});
+})
 
-module.exports = mongoose.model("Person", personSchema);
+module.exports = mongoose.model('Person', personSchema)
